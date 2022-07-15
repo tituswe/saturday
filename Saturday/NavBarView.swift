1c5fab296304646b9c42bb619d2f23a7256d06e6
tituswe <97359862+tituswe@users.noreply.github.com>
2022-07-15 16:02:13 +0800

    Add files via upload

---
+++ Saturday/NavBarView.swift
@@ -0,0 +1,74 @@
+//
+//  NavBarView.swift
+//  Saturday
+//
+//  Created by Titus Lowe on 15/7/22.
+//
+import SwiftUI
+
+struct NavBarView: View {
+    /// View of top left button.
+    let topLeftButtonView: String
+    /// View of top right button.
+    let topRightButtonView: String
+    /// Title.
+    let titleString: String
+    /// Top left button action function.
+    var topLeftButtonAction: () -> ()
+    /// Top right button action function.
+    var topRightButtonAction: () -> ()
+    
+    /// Home Nav bar view.
+    var body: some View {
+            
+            VStack(spacing: 20) {
+                
+                HStack {
+                    
+                    if topLeftButtonView != "" {
+                        Button(action: topLeftButtonAction) {
+                            Image(systemName: topLeftButtonView)
+                                .font(.title3)
+                                .foregroundColor(Color.background)
+                        }
+                    } else {
+                        Text("")
+                            .font(.title2)
+                    }
+                    
+                    Spacer()
+                    
+                    Text(titleString)
+                        .font(.title3)
+                        .foregroundColor(Color.background)
+                    
+                    Spacer()
+                    
+                    if topRightButtonView != "" {
+                        Button(action: topRightButtonAction) {
+                            Image(systemName: topRightButtonView)
+                                .font(.title3)
+                                .foregroundColor(Color.background)
+                        }
+                    } else {
+                        Text("")
+                            .font(.title2)
+                    }
+                    
+                }
+                .foregroundColor(Color.primary)
+                .padding(.top)
+                
+            }
+            .padding(.horizontal, 24)
+            .padding(.bottom, 8)
+            
+    }
+    
+}
+
+struct NavBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavBarView(topLeftButtonView: "line.horizontal.3", topRightButtonView: "circle.dashed", titleString: "Saturday", topLeftButtonAction: {}, topRightButtonAction: {})
+    }
+}
